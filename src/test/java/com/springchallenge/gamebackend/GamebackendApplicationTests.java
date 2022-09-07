1ed8e2ad8fb5099587361e14704c8f17ea2965f4
juanvalag <juanvalag@unisabana.edu.co>
2022-09-07 17:53:49 -0500

    Initial project setup

---
+++ src/test/java/com/springchallenge/gamebackend/GamebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springchallenge.gamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
