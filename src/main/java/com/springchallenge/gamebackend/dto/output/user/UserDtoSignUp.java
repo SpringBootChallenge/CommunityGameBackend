7703fc876bff3ced58a64e0754636b4bedc6a606
Laura García <60012037+laura-gar@users.noreply.github.com>
2022-09-14 11:37:16 -0500

    Feature/create user (#5)
    * Advances on User Service
    
    * Create user service is ready for testing
    
    * Package refactoring
    
    Co-authored-by: Laura Garcia <Laura.Garcia@endava.com>

---
+++ src/main/java/com/springchallenge/gamebackend/dto/output/user/UserDtoSignUp.java
@@ -0,0 +1,11 @@
+package com.springchallenge.gamebackend.dto.output.user;
+
+import lombok.Data;
+
+@Data
+public class UserDtoSignUp {
+    private String id;
+    private String email;
+    private String username;
+
+}
