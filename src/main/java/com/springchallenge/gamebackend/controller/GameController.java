5f3ee5e9b09aae84028b3d3ff0332d183e42937d
juanvalag <juanvalag@unisabana.edu.co>
2022-09-14 15:01:40 -0500

    Adding endpoint configuration for filter parameters

--- src/main/java/com/springchallenge/gamebackend/controller/GameController.java
+++ src/main/java/com/springchallenge/gamebackend/controller/GameController.java
@@ -1,11 +1,15 @@
 package com.springchallenge.gamebackend.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +33,22 @@ public class GameController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void loadGamesFromCSV() {
         gameService.loadGamesFromCSV();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<GameDto>> getGames(@RequestParam Integer page,
+            @RequestParam(required = false) Integer limit, @RequestParam(required = false) String sort,
+            @RequestParam(required = false) String title, @RequestParam(required = false) String platform,
+            @RequestParam(required = false) String genre) {
+        if (page == null) {
+            page = 1;
+        }
+        if (limit == null) {
+            limit = 10;
+        }
+        if (sort == null) {
+            sort = "newest";
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
     }
 }
