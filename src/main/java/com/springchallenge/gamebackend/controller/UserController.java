46a5219ef04744829a9fc4a03654b84340a2c604
Laura Garcia <Laura.Garcia@endava.com>
2022-09-15 09:39:50 -0500

    Login Service done

--- src/main/java/com/springchallenge/gamebackend/controller/UserController.java
+++ src/main/java/com/springchallenge/gamebackend/controller/UserController.java
@@ -1,16 +1,19 @@
 package com.springchallenge.gamebackend.controller;
 
-import org.modelmapper.ModelMapper;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.springchallenge.gamebackend.model.User;
 import com.springchallenge.gamebackend.dto.input.user.UserDto;
 import com.springchallenge.gamebackend.service.user.UserService;
+import com.springchallenge.gamebackend.exception.ExceptionType;
+import com.springchallenge.gamebackend.dto.input.user.UserLoginDto;
+import com.springchallenge.gamebackend.exception.ExceptionsGenerator;
 import com.springchallenge.gamebackend.dto.output.user.UserDtoSignUp;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
@@ -18,12 +21,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
-    public ResponseEntity<UserDtoSignUp> createUser(@RequestBody UserDto userDto){
-        User user = new User(userDto);
-        userService.createUser(user);
-        ModelMapper modelMapper = new ModelMapper();
-        UserDtoSignUp userDtoSignUp = modelMapper.map(user, UserDtoSignUp.class);
+    @PostMapping("/create")
+    public ResponseEntity<UserDtoSignUp> createUser(@RequestBody @Valid UserDto userDto, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) throw ExceptionsGenerator.getException(ExceptionType.INVALID_OBJECT, "Incorrectly formed request");
+        UserDtoSignUp userDtoSignUp= userService.createUser(userDto);
         return new ResponseEntity<>(userDtoSignUp, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public  ResponseEntity<UserDtoSignUp> loginUser(@RequestBody UserLoginDto userLoginDto){
+        UserDtoSignUp userDtoSignUp = userService.loginUser(userLoginDto);
+        return new ResponseEntity<>(userDtoSignUp, HttpStatus.OK);
     }
 }
--- src/main/java/com/springchallenge/gamebackend/service/user/UserServiceImpl.java
+++ src/main/java/com/springchallenge/gamebackend/service/user/UserServiceImpl.java
@@ -1,12 +1,18 @@
 package com.springchallenge.gamebackend.service.user;
 
+import com.springchallenge.gamebackend.dto.input.user.UserDto;
+import com.springchallenge.gamebackend.dto.output.user.UserDtoSignUp;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.springchallenge.gamebackend.model.User;
 import com.springchallenge.gamebackend.exception.ExceptionType;
 import com.springchallenge.gamebackend.repository.UserRepository;
+import com.springchallenge.gamebackend.dto.input.user.UserLoginDto;
 import com.springchallenge.gamebackend.exception.ExceptionsGenerator;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,19 +21,54 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Override
-    public User createUser(User user) {
-        Boolean userByEmail= userRepository.findByEmail(user.getEmail()) == null;
-        Boolean userByUsername= userRepository.findByUsername(user.getUsername()) == null;
+    public UserDtoSignUp createUser(UserDto userDto) {
+        User user = new User(userDto);
+        Boolean userByEmail = userRepository.findByEmail(user.getEmail()) == null;
+        Boolean userByUsername = userRepository.findByUsername(user.getUsername()) == null;
         if(userByEmail && userByUsername){
             userRepository.save(user);
-            return user;
-        }else{
-            throw ExceptionsGenerator.getException(ExceptionType.DUPLICATE_ENTITY, "Email or username in use.");
+            ModelMapper modelMapper = new ModelMapper();
+            UserDtoSignUp userDtoSignUp = modelMapper.map(user, UserDtoSignUp.class);
+            return userDtoSignUp;
         }
+        throw ExceptionsGenerator.getException(ExceptionType.DUPLICATE_ENTITY, "Email or username in use.");
     }
 
     @Override
     public User findById(String id) {
-        return userRepository.findById(id).get();
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return userRepository.findById(id).get();
+        }
+        throw ExceptionsGenerator.getException(ExceptionType.NOT_FOUND,
+                "There is no user with the supplied id.");
+    }
+
+    @Override
+    public UserDtoSignUp loginUser(UserLoginDto userLoginDto) {
+        String username = userLoginDto.getUsername();
+        User userExists = userRepository.findByUsername(username);
+        if(userExists != null){
+            if(reviewPassword(userExists.getPassword(), userLoginDto.getPassword())){
+                userExists.login();
+                userRepository.save(userExists);
+                ModelMapper modelMapper = new ModelMapper();
+                UserDtoSignUp userDtoSignUp = modelMapper.map(userExists, UserDtoSignUp.class);
+                return userDtoSignUp;
+            }
+            throw ExceptionsGenerator.getException(ExceptionType.INVALID_CREDENTIALS,
+                    "Invalid credentials");
+        }
+        throw ExceptionsGenerator.getException(ExceptionType.NOT_FOUND,
+                "There is no user with this username.");
+    }
+
+    public boolean isLogged(String userId) {
+        User user = findById(userId);
+        return user.isLogged();
+    }
+
+    private boolean reviewPassword(String dbPassword, String userPassword){
+        return dbPassword.equals(userPassword);
     }
 }
