a9d97c36effcab4689da8fc4310d4cff490ca15d
juanvalag <juanvalag@unisabana.edu.co>
2022-09-16 13:16:26 -0500

    Support to OpenApi 3

--- src/main/java/com/springchallenge/gamebackend/config/SwaggerConfig.java
+++ src/main/java/com/springchallenge/gamebackend/config/SwaggerConfig.java
@@ -1,40 +1,19 @@
 package com.springchallenge.gamebackend.config;
 
-import jdk.jshell.SourceCodeAnalysis;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
 //
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket swaggerApi(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springchallenge.gamebackend.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                ;
-    }
-
-    private ApiInfo apiInfo(){
-        return new ApiInfoBuilder().title("Game Community - REST API")
-                .description("Spring Boot application for the gaming community")
-                .contact(new Contact("Laura Garcia", "https://github.com/laura-gar",  "laura.garcia@endava.com"))
-//                .contact(new Contact("Juan David Valencia", "https://github.com/juanvalag2019",  "juan.valencia@endava.com"))
-                .version("1.0")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Game Community - REST API").description(
+                        "Spring Boot application for the gaming community"));
     }
 
 
