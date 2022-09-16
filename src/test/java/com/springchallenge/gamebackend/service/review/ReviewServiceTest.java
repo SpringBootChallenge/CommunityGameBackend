efa923fef0d9fdeb2753c8b9ccd5949b3377e36d
juanvalag <juanvalag@unisabana.edu.co>
2022-09-16 11:31:00 -0500

    Testing findById with existent review

--- src/test/java/com/springchallenge/gamebackend/service/review/ReviewServiceTest.java
+++ src/test/java/com/springchallenge/gamebackend/service/review/ReviewServiceTest.java
@@ -154,4 +154,16 @@ public class ReviewServiceTest {
         assertEquals( "There is no review with the supplied id.", exception.getMessage());
     }
 
+    @Test
+    void findById_existentReview_returnReview() {
+        // Arrange
+        String reviewId = "00018a93-51a7-4928-b869-77a2a75695fb";
+        Review expectedReview= Mockito.mock(Review.class);
+        when(reviewRepo.findById(reviewId)).thenReturn(Optional.of(expectedReview));
+        // act
+        Review actualReview=reviewService.findById(reviewId);
+        // assert
+        assertEquals( expectedReview,actualReview);
+    }
+
 }
