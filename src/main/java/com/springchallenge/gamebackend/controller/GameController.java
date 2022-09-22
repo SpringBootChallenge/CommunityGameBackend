9a3775c146651bc9c980efaa1c7ca341611128d3
juanvalag <juanvalag@unisabana.edu.co>
2022-09-22 17:31:53 -0500

    Added the PUT mapping to set the game state instead of POST

--- src/main/java/com/springchallenge/gamebackend/controller/GameController.java
+++ src/main/java/com/springchallenge/gamebackend/controller/GameController.java
@@ -6,15 +6,7 @@ import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.springchallenge.gamebackend.dto.input.game.GameFilterCriteria;
 import com.springchallenge.gamebackend.dto.input.gamestate.GameStateDto;
@@ -55,7 +47,7 @@ public class GameController {
                 HttpStatus.OK);
     }
 
-    @PostMapping("/{gameId}/state")
+    @PutMapping("/{gameId}/state")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void setGameState(@RequestHeader(value = "user-id", required = true) String userIdHeader,
             @PathVariable String gameId, @RequestBody GameStateDto state) {
